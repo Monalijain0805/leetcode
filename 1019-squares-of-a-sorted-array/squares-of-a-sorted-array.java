853982148606bc313e4f2475205beafbc76a12d1
Monalijain0805 <jainmonali0805@gmail.com>
2026-04-30 19:04:09 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (75.45%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -10,13 +10,12 @@
 //     }
 // }
 
+//USING TEO POINTERS- left ko aage se shruu kia right ko piche se shruru kia while loop lgake dekha agr left squares right squares se chota h toh resultant m nums of left into left  bhrdo or left ko plus plus krdo taaki aage wala check ho else right mtlb piche wale ka squares leke array m bhrdo or right-- krdo
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-
         int left = 0, right = n - 1, index = n - 1;
-
         while (left <= right) {
             if (nums[left] * nums[left] > nums[right] * nums[right]) {
                 res[index] = nums[left] * nums[left];
