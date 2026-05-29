ab0a238167c1bfdd8bfa8ded13c64c7f97d99878
Monalijain0805 <jainmonali0805@gmail.com>
2026-05-29 23:02:24 +0530

    Time: 1 ms (98.27%) | Memory: 47.5 MB (79.12%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,76 +1,52 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-// //         int m=arr.length;
-// //         int n=arr[0].length;
-// //         int[][] helping_array= new int[m][n];
-// //         for(int i=0;i<m;i++){
-// //             for(int j=0;j<n;j++){
-// //                 helping_array[i][j]=arr[i][j];
-// //             }
-// //         }
-// //         for(int i=0;i<m;i++){
-// //             for(int j=0;j<n;j++){
-// //                 if(helping_array[i][j]==0){
-// //                     for(int a=0;a<n;a++){
-// //                         arr[i][a]=0;
-// //                     }
-// //                     for(int b=0;b<m;b++){
-// //                         arr[b][j]=0;
-// //                     }
-// //                 }
-// //             }
-// //         }
-// //     }
-// // }
-// //TC IS M*N
-
-
-
-
-// class Solution {
-//     public void setZeroes(int[][] arr) {
-//         int m=arr.length;
-//         int n=arr[0].length;
-//         int[][] helping_array= new int[m][n];
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 helping_array[i][j]=arr[i][j];
-//             }
-//         }
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(helping_array[i][j]==0){
-//                     for(int a=0;a<n;a++){
-//                         arr[i][a]=0;
-//                     }
-//                     for(int b=0;b<m;b++){
-//                         arr[b][j]=0;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-int m=arr.length;
-int n=arr[0].length;
-int[][]helperArray=new int[m][n];
-for(int i=0;i<m;i++){
-    for(int j=0;j<n;j++){
-        helperArray[i][j]=arr[i][j];
-    }
-}
-for(int i=0;i<m;i++){
-    for(int j=0;j<n;j++){
-        if(helperArray[i][j]==0){
-            for(int b=0;b<m;b++){
-                arr[b][j]=0;
-            }
-            for(int a=0;a<n;a++){
-                arr[i][a]=0;
+        int m = arr.length, n =arr[0].length;
+        boolean zerorow =false;
+        boolean zerocol = false;
+        for(int z =0;z<n;z++){
+            if(arr[0][z]==0){
+                zerorow = true;
             }
         }
+          for(int x =0;x<m;x++){
+            if(arr[x][0]==0){
+                zerocol= true;
+            }
+        }
+        for (int i =1;i<m;i++){
+            for(int j =1;j<n;j++){
+               if(arr[i][j]==0){
+                arr[0][j]=0;
+                arr[i][0]=0; 
+            }
+        }        
     }
-}
+    for(int a=1;a<n;a++){
+                if(arr[0][a]==0){
+                   for(int b=0;b<m;b++){
+                    arr[b][a]=0;
+                    
+                   } 
+                }
+    }
+                   for(int c=1;c<m;c++){
+                if(arr[c][0]==0){
+                   for(int d=0;d<n;d++){
+                    arr[c][d]=0;
+                    
+                }
+            }
+        }
+ if(zerorow==true){
+    for(int j =0;j<n;j++){
+        arr[0][j]=0;
+    }
  }
+
+    if(zerocol==true){
+    for(int i =0;i<m;i++){
+        arr[i][0]=0;
+    }
+ }
+    }
 }
