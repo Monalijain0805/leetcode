70b46c15f7b3d60b5058e99a9cb4f39a055173b5
Monalijain0805 <jainmonali0805@gmail.com>
2026-06-17 21:35:17 +0530

    Time: 10 ms (33.88%) | Memory: 47.3 MB (63.96%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,32 +1,44 @@
+// // class Solution {
+// //     public int[] sortedSquares(int[] nums) {
+// //         int[] newarr = new int[nums.length];
+// //         for(int i =0 ;i< nums.length ; i++){
+// //             nums[i]= nums[i] * nums[i];
+// //             newarr[i] = nums[i];
+// //         }
+// //         Arrays.sort(newarr);
+// //         return newarr;
+// //     }
+// // }
+
+
+
+
+// //USING TEO POINTERS- left ko aage se shruu kia right ko piche se shruru kia while loop lgake dekha agr left squares right squares se chota h toh resultant m nums of left into left  bhrdo or left ko plus plus krdo taaki aage wala check ho else right mtlb piche wale ka squares leke array m bhrdo or right-- krdo
 // class Solution {
 //     public int[] sortedSquares(int[] nums) {
-//         int[] newarr = new int[nums.length];
-//         for(int i =0 ;i< nums.length ; i++){
-//             nums[i]= nums[i] * nums[i];
-//             newarr[i] = nums[i];
+//         int n = nums.length;
+//         int[] res = new int[n];
+//         int left = 0, right = n - 1, index = n - 1;
+//         while (left <= right) {
+//             if (nums[left] * nums[left] > nums[right] * nums[right]) {
+//                 res[index] = nums[left] * nums[left];
+//                 left++;
+//             } 
+//             else {
+//                 res[index] = nums[right] * nums[right];
+//                 right--;
+//             }
+//             index--;
 //         }
-//         Arrays.sort(newarr);
-//         return newarr;
+//         return res;
 //     }
 // }
-
-//USING TEO POINTERS- left ko aage se shruu kia right ko piche se shruru kia while loop lgake dekha agr left squares right squares se chota h toh resultant m nums of left into left  bhrdo or left ko plus plus krdo taaki aage wala check ho else right mtlb piche wale ka squares leke array m bhrdo or right-- krdo
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        int left = 0, right = n - 1, index = n - 1;
-        while (left <= right) {
-            if (nums[left] * nums[left] > nums[right] * nums[right]) {
-                res[index] = nums[left] * nums[left];
-                left++;
-            } else {
-                res[index] = nums[right] * nums[right];
-                right--;
-            }
-            index--;
+        for(int i =0;i<nums.length;i++){
+            nums[i] *= nums[i];
         }
-
-        return res;
+        Arrays.sort(nums);
+        return nums;
     }
 }
