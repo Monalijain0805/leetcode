795ddf376b1438e35d5c4b85e4b09956f4134e77
Monalijain0805 <jainmonali0805@gmail.com>
2026-06-25 16:30:28 +0530

    Time: 33 ms (77.18%) | Memory: 75.9 MB (7.71%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -109,56 +109,90 @@
 
 
 
+// class Solution {
+//     static int ans;
+
+//     public void swap(int[] arr, int i, int j) {
+//         int temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//     }
+
+//     public int partition(int[] arr, int low, int high) {
+//         int pivot = arr[low];   // \U0001f511 pivot = first element
+//         int pivotindex = low;
+
+//         int greaterCount = 0;
+//         for (int i = low + 1; i <= high; i++) {
+//             if (arr[i] >= pivot) greaterCount++; // count bade elements
+//         }
+
+//         int correctindex = low + greaterCount;  // pivot ki sahi jagah (descending)
+//         swap(arr, pivotindex, correctindex);
+
+//         int i = low, j = high;
+//         while (i < correctindex && j > correctindex) {
+//             if (arr[i] >= pivot) i++;
+//             else if (arr[j] < pivot) j--;
+//             else {
+//                 swap(arr, i, j);
+//                 i++;
+//                 j--;
+//             }
+//         }
+//         return correctindex;
+//     }
+
+//     public void quickselect(int[] arr, int low, int high, int k) {
+//         if (low > high) return;
+
+//         int index = partition(arr, low, high);
+
+//         if (index == k - 1) {  // \U0001f511 directly kth largest
+//             ans = arr[index];
+//             return;
+//         }
+//         if (k - 1 < index) quickselect(arr, low, index - 1, k);
+//         else quickselect(arr, index + 1, high, k);
+//     }
+
+//     public int findKthLargest(int[] arr, int k) {
+//         ans = -1;
+//         quickselect(arr, 0, arr.length - 1, k);
+//         return ans;
+//     }
+// }
+
+
+
+
+
+
+//USING SORTING
 class Solution {
-    static int ans;
-
-    public void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    public int partition(int[] arr, int low, int high) {
-        int pivot = arr[low];   // \U0001f511 pivot = first element
-        int pivotindex = low;
-
-        int greaterCount = 0;
-        for (int i = low + 1; i <= high; i++) {
-            if (arr[i] >= pivot) greaterCount++; // count bade elements
-        }
-
-        int correctindex = low + greaterCount;  // pivot ki sahi jagah (descending)
-        swap(arr, pivotindex, correctindex);
-
-        int i = low, j = high;
-        while (i < correctindex && j > correctindex) {
-            if (arr[i] >= pivot) i++;
-            else if (arr[j] < pivot) j--;
-            else {
-                swap(arr, i, j);
-                i++;
-                j--;
-            }
-        }
-        return correctindex;
-    }
-
-    public void quickselect(int[] arr, int low, int high, int k) {
-        if (low > high) return;
-
-        int index = partition(arr, low, high);
-
-        if (index == k - 1) {  // \U0001f511 directly kth largest
-            ans = arr[index];
-            return;
-        }
-        if (k - 1 < index) quickselect(arr, low, index - 1, k);
-        else quickselect(arr, index + 1, high, k);
-    }
-
-    public int findKthLargest(int[] arr, int k) {
-        ans = -1;
-        quickselect(arr, 0, arr.length - 1, k);
-        return ans;
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
     }
 }
+
+
+
+
+
+
+
+// //HEAP CONCEPT
+// class Solution{
+//     public class 
+// }
+// PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+// for (int num : arr) {
+
+//     minHeap.offer(num);
+
+//     if (minHeap.size() > k) {
+//         minHeap.poll();
+//     }
+// }
