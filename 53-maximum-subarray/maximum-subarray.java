a0848c4e14a9a1a84fbf0e46569794e57f2857fe
Monalijain0805 <jainmonali0805@gmail.com>
2026-07-16 19:50:58 +0530

    Time: 1 ms (99.97%) | Memory: 77.5 MB (14.14%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxx= Integer.MIN_VALUE;
+        int sum=0;
+        for(int i =0;i<nums.length;i++){
+            sum = sum+nums[i];
+            maxx= Math.max(maxx, sum);
+            if(sum > maxx){
+                maxx= sum;
+            }
+            if(sum <0){
+                sum=0;
+            }
+        }
+        return maxx;
+        
+    }
+}
