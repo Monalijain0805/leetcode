2e4df66b3ea0f8f0319261576b6a73606d4af454
Monalijain0805 <jainmonali0805@gmail.com>
2026-07-20 23:17:57 +0530

    Time: 6 ms (15.03%) | Memory: 46.2 MB (79.33%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,17 +1,27 @@
 class Solution {
     public int longestValidParentheses(String s) {
+
+        // Stack stores the INDEX of '('
         Stack<Integer> st = new Stack<>();
 
         // Push -1 as a base index
         // This helps calculate length when valid substring starts from index 0
         st.push(-1);
+
+        // Stores the maximum valid length found so far
         int maxLength = 0;
+
+        // Traverse the entire string
         for (int i = 0; i < s.length(); i++) {
+
+            // If current character is '('
             if (s.charAt(i) == '(') {
+
                 // Store its index in the stack
                 st.push(i);
-            } 
-            else { // Current character is ')'
+
+            } else { // Current character is ')'
+
                 // Remove one matching '(' index
                 st.pop();
 
@@ -22,8 +32,7 @@ class Solution {
                     // Store this ')' index as the new base
                     st.push(i);
 
-                } 
-                else {
+                } else {
 
                     // Current valid length
                     // = Current Index - Previous Unmatched Index
