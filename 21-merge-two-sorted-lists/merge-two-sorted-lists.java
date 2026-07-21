8880cd4e6525e54a1f11fed16c1e2bc66641e1a7
Monalijain0805 <jainmonali0805@gmail.com>
2026-07-21 16:26:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (57.89%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,38 +1,67 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       ListNode dummy = new ListNode(100);
-       ListNode t1 = list1;
-       ListNode t2 = list2;
-       ListNode t = dummy;
-       while(t1!= null && t2!= null){
-        if(t1.val<=t2.val){
-            t.next = t1;
-            t1= t1.next;
-            t = t.next;
-        }
-        else {
-            t.next = t2;
-            t2 = t2.next;
-            t= t.next;
-        }
-       }
-       if(t1 == null){
-            t.next = t2;
-        }
-        else{
-            t.next = t1;
-        }
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode dummy = new ListNode(100);
+//        ListNode t1 = list1;
+//        ListNode t2 = list2;
+//        ListNode t = dummy;
+//        while(t1!= null && t2!= null){
+//         if(t1.val<=t2.val){
+//             t.next = t1;
+//             t1= t1.next;
+//             t = t.next;
+//         }
+//         else {
+//             t.next = t2;
+//             t2 = t2.next;
+//             t= t.next;
+//         }
+//        }
+//        if(t1 == null){
+//             t.next = t2;
+//         }
+//         else{
+//             t.next = t1;
+//         }
        
 
-    return dummy.next; }
-}
+//     return dummy.next; }
+// }
+
+
+ class Solution {
+   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    ListNode dummy = new ListNode(100);
+    ListNode t1= list1;
+    ListNode t2= list2;
+    ListNode t= dummy;
+    while(t1!=null && t2!=null){
+        if(t1.val<=t2.val){
+            t.next= t1;
+            t1= t1.next;
+            t= t.next;
+        }
+        else{
+            t.next= t2;
+            t2= t2.next;
+            t= t.next;
+        }
+    }
+    if(t1==null){
+        t.next=t2;
+    }
+    if(t2==null){
+        t.next= t1;
+    }
+    return dummy.next;
+   }
+ }
