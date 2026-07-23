86ef3ce6b6864ca9df93b2d3fec1da02cbf4ffca
Monalijain0805 <jainmonali0805@gmail.com>
2026-07-24 00:39:48 +0530

    Time: 15 ms (27.72%) | Memory: 52.6 MB (40.54%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         int times = nums.length / 3;
+//         List<Integer> ans = new ArrayList<>();
+//         for (int i = 0; i <nums.length; i++) {
+//             int count = 0;
+//             for (int j = 0; j < nums.length; j++) {
+//                 if (nums[i] == nums[j]) {
+//                     count++;
+//                 }
+//             }
+//             if (count > times && !ans.contains(nums[i])) {
+//                 ans.add(nums[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// }THIS IS o(n^2) approach with o(n)space complexity
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int times = nums.length / 3;
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i <nums.length; i++) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
             int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
+            if (map.containsKey(num)) {
+                count = map.get(num);
             }
-            if (count > times && !ans.contains(nums[i])) {
-                ans.add(nums[i]);
+            //else condition
+            map.put(num, count + 1);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int key : map.keySet()) {
+            int count = map.get(key);
+            if (count > nums.length / 3) {
+                ans.add(key);
             }
         }
         return ans;
