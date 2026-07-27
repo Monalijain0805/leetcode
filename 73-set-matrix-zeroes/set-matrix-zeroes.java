c3ddd1442355177241370b4df4c7833174b154fc
Monalijain0805 <jainmonali0805@gmail.com>
2026-07-28 00:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (91.98%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,52 +1,72 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-        int m = arr.length, n =arr[0].length;
-        boolean zerorow =false;
+
+        // Number of rows and columns
+        int m = arr.length;
+        int n = arr[0].length;
+
+        // Check whether first row or first column
+        // originally contains any 0
+        boolean zerorow = false;
         boolean zerocol = false;
-        for(int z =0;z<n;z++){
-            if(arr[0][z]==0){
+
+        // Check first row
+        for (int j = 0; j < n; j++) {
+            if (arr[0][j] == 0) {
                 zerorow = true;
             }
         }
-          for(int x =0;x<m;x++){
-            if(arr[x][0]==0){
-                zerocol= true;
-            }
-        }
-        for (int i =1;i<m;i++){
-            for(int j =1;j<n;j++){
-               if(arr[i][j]==0){
-                arr[0][j]=0;
-                arr[i][0]=0; 
-            }
-        }        
-    }
-    for(int a=1;a<n;a++){
-                if(arr[0][a]==0){
-                   for(int b=0;b<m;b++){
-                    arr[b][a]=0;
-                    
-                   } 
-                }
-    }
-                   for(int c=1;c<m;c++){
-                if(arr[c][0]==0){
-                   for(int d=0;d<n;d++){
-                    arr[c][d]=0;
-                    
-                }
-            }
-        }
- if(zerorow==true){
-    for(int j =0;j<n;j++){
-        arr[0][j]=0;
-    }
- }
 
-    if(zerocol==true){
-    for(int i =0;i<m;i++){
-        arr[i][0]=0;
-    }
- }
+        // Check first column
+        for (int i = 0; i < m; i++) {
+            if (arr[i][0] == 0) {
+                zerocol = true;
+            }
+        }
+
+        // Use first row and first column as markers
+        // If arr[i][j] is 0, mark its row and column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == 0) {
+                    arr[0][j] = 0;   // Mark this column
+                    arr[i][0] = 0;   // Mark this row
+                }
+            }
+        }
+
+        // Make marked columns zero
+        for (int j = 1; j < n; j++) {
+            if (arr[0][j] == 0) {
+                for (int i = 0; i < m; i++) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        // Make marked rows zero
+        for (int i = 1; i < m; i++) {
+            if (arr[i][0] == 0) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        // If first row originally had a 0,
+        // make the whole first row 0
+        if (zerorow) {
+            for (int j = 0; j < n; j++) {
+                arr[0][j] = 0;
+            }
+        }
+
+        // If first column originally had a 0,
+        // make the whole first column 0
+        if (zerocol) {
+            for (int i = 0; i < m; i++) {
+                arr[i][0] = 0;
+            }
+        }
     }
 }
